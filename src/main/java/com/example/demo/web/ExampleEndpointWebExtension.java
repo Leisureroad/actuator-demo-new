348917283636665567db8b4c9e119bcacf36698a
Fan Liu <charles_liuf@hotmail.com>
2018-04-28 12:11:12 +0800

    添加web extension

---
+++ src/main/java/com/example/demo/web/ExampleEndpointWebExtension.java
@@ -0,0 +1,21 @@
+package com.example.demo.web;
+
+import com.example.demo.ExampleEndpoint;
+import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.boot.actuate.endpoint.web.WebEndpointResponse;
+import org.springframework.boot.actuate.endpoint.web.annotation.EndpointWebExtension;
+
+@EndpointWebExtension(endpoint = ExampleEndpoint.class)
+public class ExampleEndpointWebExtension {
+
+	private final ExampleEndpoint delegate;
+
+	public ExampleEndpointWebExtension(ExampleEndpoint delegate) {
+		this.delegate = delegate;
+	}
+
+	@ReadOperation
+	public WebEndpointResponse<String> read() {
+		return new WebEndpointResponse<>("Hello web world");
+	}
+}
